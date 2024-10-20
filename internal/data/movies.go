80e13e92b2b9da757329add0b99d50df91ff2f81
Nik V <nvukovlj@gmail.com>
2024-10-20 19:20:24 +0200

    added special formatting for runtime output

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -9,7 +9,7 @@ type Movie struct {
 	CreatedAt time.Time `json:"-"`                 // timestamp when the movie is added to the DB
 	Title     string    `json:"title"`             // movie title
 	Year      int32     `json:"year,omitempty"`    // movie release year
-	Runtime   int32     `json:"runtime,omitempty"` // movie runtime (minutes)
+	Runtime   Runtime   `json:"runtime,omitempty"` // movie runtime (minutes)
 	Genres    []string  `json:"genres,omitempty"`  // genres for the movie (romance, comedy, etc)
 	Version   int32     `json:"version"`           // version number, incremented every time movie information updated
 }
