c28a283099748d3bf62bdb633a385fdabc2678ca
nikvuk <nvukovlj@gmail.com>
2025-10-11 22:24:56 +0200

    Initial implementation of sending of welcome email when user created

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/nikvuk/greenlight/internal/data"
 	"github.com/nikvuk/greenlight/internal/jsonlog"
+	"github.com/nikvuk/greenlight/internal/mailer"
 
 	_ "github.com/lib/pq"
 )
@@ -29,12 +30,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -52,6 +61,12 @@ func main() {
 	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "d260e3a9e5485a", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "3b7d34391714f4", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlightvukovljak.org>", "SMTP sender")
 
 	flag.Parse()
 
@@ -71,6 +86,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
